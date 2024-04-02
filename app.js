20908af91eb404b61c6161424126ba5fe7e93e51
Eduardo Capdeville <capdevilleeduardo@gmail.com>
2024-04-02 18:51:43 -0300

    Alterações no arquivos html, css e javascript para novas funcionalidades e para finalização do projeto.

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 var buttonClickCount = 0;
+var rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+var rainbowIndex = 0;
+var rainbowInterval;
 
 function increment() {
     var counterElement = document.getElementById('counter');
     var counterValue = parseInt(counterElement.innerText);
 
     buttonClickCount++;
+    counterElement.innerText = counterValue + 1;
 
     if (buttonClickCount === 100) {
-        alert("Nossa, você deve ser bem desocupado pra apertar esse botão 100 vezes...");
-        buttonClickCount = 0;
+        startRainbowAnimation();
     }
-    
-    counterElement.innerText = counterValue + 1;
 }
+
+function startRainbowAnimation() {
+    rainbowInterval = setInterval(function() {
+        var counterElement = document.getElementById('counter');
+        counterElement.style.color = rainbowColors[rainbowIndex];
+        rainbowIndex = (rainbowIndex + 1) % rainbowColors.length;
+    }, 100);
+}
+
+// Evento de clique para o botão de dica
+const tipButton = document.querySelector('.tip');
+tipButton.addEventListener('click', function() {
+    alert("Clique 100 vezes no botão e veja algo incrível acontecer.");
+});
